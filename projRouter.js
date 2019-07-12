848fad0c016311bf4a6510c7b5ea8cc99057b07b
Shannon <shannon.yoshikawa@gmail.com>
2019-07-12 11:07:04 -0700

    action endponts created and tested

--- projRouter.js
+++ projRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const ProjDB = require("./data/helpers/projectModel");
+const auth = require("./auth/mw");
 const router = express.Router();
+
 //tested
 router.get("/", async (req, res) => {
   try {
@@ -29,74 +31,48 @@ router.get("/:id", async (req, res) => {
   }
 });
 //tested
-router.get("/:id/actions", validateProjId, async (req, res)=> {
-    const {id} = req.params
-    try{
-        const actions = await ProjDB.getProjectActions(id)
-        if (actions.length) {
-            res.status(200).json(actions)
-        } else {
-            res.status(404).json({message: "Found no actions for this project"})
-        }
-    } catch(error) {
-        res.status(500).json({message: "Error retrieving project actions"})
-    }
-})
-//tested
-router.post("/", validateProj, async (req, res)=> {
-    try{
-        const project = await ProjDB.insert(req.body);
-        res.status(201).json(project)
-    } catch(error) {
-        res.status(500).json({message: "Error adding project"})
-    }
-})
-//tested
-router.put("/:id", validateProj, validateProjId, async (req, res)=> {
-    try{
-        const project = await ProjDB.update(req.params.id, req.body)
-        if (project) {
-            res.status(201).json(project)
-        }
-    } catch(error) {
-        res.status(500).json({message: "Error updating project"})
-    }
-})
-//tested
-router.delete("/:id", validateProjId, async(req, res)=> {
-    const {id} = req.params
-    try{
-        await ProjDB.remove(id)
-        res.status(204).end()
-    } catch(error) {
-        res.status(500).json({message: "Error deleting project"})
-    }
-})
-
-async function validateProj(req, res, next) {
-    if(!req.body) {
-        res.status(400).json({message: "Missing project data"})
-    } else if (!req.body.name) {
-        res.status(400).json({message: "Missing project name"})
-    } else if (!req.body.description) {
-        res.status(400).json({message: "Missing project description"})
-    } else {
-        next();
-    }
-}
-
-async function validateProjId(req, res, next) {
+router.get("/:id/actions", auth.validateProjId, async (req, res) => {
   const { id } = req.params;
   try {
-    const project = await ProjDB.get(id);
-    if (project) {
-      next();
+    const actions = await ProjDB.getProjectActions(id);
+    if (actions.length) {
+      res.status(200).json(actions);
     } else {
-      res.status(404).json({ message: "Invalid project ID" });
+      res.status(404).json({ message: "Found no actions for this project" });
     }
   } catch (error) {
-    res.status(500).json({ message: "Error retrieving project" });
+    res.status(500).json({ message: "Error retrieving project actions" });
   }
-}
+});
+//tested
+router.post("/", auth.validateProj, async (req, res) => {
+  try {
+    const project = await ProjDB.insert(req.body);
+    res.status(201).json(project);
+  } catch (error) {
+    res.status(500).json({ message: "Error adding project" });
+  }
+});
+//tested
+router.put("/:id", auth.validateProj, auth.validateProjId, async (req, res) => {
+  try {
+    const project = await ProjDB.update(req.params.id, req.body);
+    if (project) {
+      res.status(201).json(project);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error updating project" });
+  }
+});
+//tested
+router.delete("/:id", auth.validateProjId, async (req, res) => {
+  const { id } = req.params;
+  try {
+    await ProjDB.remove(id);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting project" });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
-const helmet = require("helmet")
+const helmet = require("helmet");
 
-const projRouter = require("./projRouter")
-const actionRouter = require("./actionRouter")
+const projRouter = require("./projRouter");
+const actionRouter = require("./actionRouter");
 
 const server = express();
-server.use(helmet())
+server.use(helmet());
 server.use(express.json());
-server.use("/projects", projRouter)
-server.use("/actions", actionRouter)
+server.use("/projects", projRouter);
+server.use("/actions", actionRouter);
 
-server.get("/", (req, res)=> {
-    res.send(`<h2>SPRINT CHALLENGE</h2><h3>nbd</h3>`)
-})
+server.get("/", logger, (req, res) => {
+  res.send(`<h2>SPRINT CHALLENGE</h2><h3>nbd</h3>`);
+});
 
-module.exports= server;
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.path}`);
+  next();
+}
+
+module.exports = server;
